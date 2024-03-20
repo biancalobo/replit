049f5a0f612477fc980e0a2dffdef9a6ff2aeeaf
biancalobo <160778094+biancalobo@users.noreply.github.com>
2024-03-20 19:15:52 +0000

    tarefas.c

---
+++ tarefas.c
@@ -0,0 +1,23 @@
+#include "tarefas.h"
+#include <stdio.h>
+
+Erro criar(Tarefa t[], int *pos) {
+  printf("função criar tarefa");
+  return OK;
+}
+Erro deletar(Tarefa t[], int *pos) {
+  printf("função deletar tarefa");
+  return OK;
+}
+Erro listar(Tarefa t[], int pos) {
+  printf("função listar tarefa");
+  return OK;
+}
+Erro salvar(Tarefa t[], int pos, int tamanho) {
+  printf("função salvar tarefa");
+  return OK;
+}
+Erro carregar(Tarefa t[], int *pos, int tamanho) {
+  printf("função carregar tarefa");
+  return OK;
+}
